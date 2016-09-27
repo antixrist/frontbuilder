d31090c8f937c87fbc81f19bee0a4d9ffcb46085
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-09-27 22:28:25 +0400

    .

---
+++ gulpfile.babel.js/index.js
@@ -0,0 +1,22 @@
+import del from 'del';
+import gulp from 'gulp';
+import gulpPlugins from 'gulp-load-plugins';
+import globWatcher from 'glob-watcher';
+import runSequence from 'run-sequence';
+import through2 from 'through2';
+const $ = gulpPlugins();
+// $.if
+// $.rename
+// $.cached
+// $.changed
+// $.newer
+// $.remember
+// $.notify
+// $.plumber
+// $.pug
+// $.sass
+// $.rename
+// $.sourcemaps
+// $.util
+// $.util
+
--- gulpfile.js/index.js
+++
@@ -1,3 +0,0 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const del = require('del');
