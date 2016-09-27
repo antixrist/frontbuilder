7ca5dba9ac31c954feadf4de4811dabd576cac81
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-09-27 21:54:39 +0400

    add gulp tasks boilerplae

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,11 +1,3 @@
-/**
- CSS assets:
- https://github.com/postcss/postcss
- https://github.com/shutterstock/postcss-copy-assets
- https://github.com/geut/postcss-copy
- https://github.com/devex-web-frontend/postcss-assets-rebase
- https://github.com/ben-eb/postcss-normalize-url
- https://github.com/geut/postcss-copy
- https://github.com/unlight/postcss-import-url
- https://www.npmjs.com/package/gulp-importify
- */
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const del = require('del');
