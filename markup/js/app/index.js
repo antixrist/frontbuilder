364dd51403243a070130882cd9489c1e0e5bb08e
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-09-30 03:55:08 +0400

    vue boilerplate...

--- markup/js/app/index.js
+++ markup/js/app/index.js
@@ -3,7 +3,22 @@ import VueResource from 'vue-resource';
 import store from './store';
 import router from './router';
 
+const API_BASE = 'http://jsonplaceholder.typicode.com';
+
 Vue.use(VueResource);
+
+Vue.http.options = {
+  root: API_BASE
+};
+
+Vue.http.interceptors.push((request, next) => {
+  next((response) => {
+    // Handle global API 404 =>
+    if (response.status === 404) {
+      router.push('/404');
+    }
+  });
+});
 
 import {sync} from 'vuex-router-sync';
 sync(store, router);
