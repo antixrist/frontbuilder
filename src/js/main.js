1c5f958c92c7c37b40b3da97b8bb02c672aeb25e
Kartashov Alexey <kartashov.al.and@yandex.ru>
2016-12-19 06:39:25 +0400

    .

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,3 @@ import '../styles/main.scss';
 import { app as vueApp, router, store } from './app';
 
 vueApp.$mount('#app');
-
-const prom = () => new Promise(resolve => setTimeout(() => resolve('asdasd'), 200));
-
-const fn = async function () {
-  
-  const qweqwe = await prom().then((str) => {
-    // console.log('str', str);
-    
-    return Promise.resolve('asdasdasd');
-  });
-  
-};
-
-fn();
