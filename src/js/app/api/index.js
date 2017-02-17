cb881d4cb0b522413166bdff50da33fe010016aa
Лёха <kartashov.al.and@yandex.ru>
2017-02-17 19:40:26 +0400

    .

--- src/js/app/api/index.js
+++ src/js/app/api/index.js
@@ -7,18 +7,18 @@ const api = http.factory({
   // withCredentials: true,
   headers: {
   //   'Accept': 'application/json',
-  //   'Content-Type': 'application/x-www-form-urlencoded'
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/x-www-form-urlencoded'
+    // 'Content-Type': 'application/json'
   },
   data: {}
 });
 
-// api.interceptors.request.use((request) => {
-//   if (request.data && request.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
-//     request.data = qs.stringify(request.data, { encode: false, arrayFormat: 'brackets' });
-//   }
-//   return request;
-// });
+api.interceptors.request.use((request) => {
+  if (request.data && request.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+    request.data = qs.stringify(request.data, { encode: false, arrayFormat: 'brackets' });
+  }
+  return request;
+});
 
 api.interceptors.request.use(req => {
   progress.start();
@@ -41,5 +41,13 @@ api.interceptors.response.use(res => {
 
 export default api;
 
-window.api = api;
-window.progress = progress;
+// window.api = api;
+// window.progress = progress;
+
+// (async function () {
+//   const { data, status, statusText, headers, config, request } = await window.api.post('/login', { login: 'test', password: 'B4mGld' });
+//
+//   const { data: { api_token } } = data;
+//   console.log(data, api_token);
+//
+// })().catch(err => console.log(err));
