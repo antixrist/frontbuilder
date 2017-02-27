522a5c3d7b0abf1e7c794950216f399400986454
Лёха <kartashov.al.and@yandex.ru>
2017-02-27 14:57:33 +0400

    .

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL;
-export const NODE_ENV = process.env.NODE_ENV;
+export const API_URL       = process.env.API_URL;
+export const NODE_ENV      = process.env.NODE_ENV;
 export const isProduction  = process.env.NODE_ENV == 'production';
 export const isDevelopment = process.env.NODE_ENV == 'development';
