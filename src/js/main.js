4345407bd3a1bdddc56088fcd721549eaea90062
Лёха <kartashov.al.and@yandex.ru>
2017-03-10 14:46:48 +0400

    >

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
-import 'babel-polyfill';
 /**
- * в точке входа лучше определить все тяжёлые зависимости,
+ * В точке входа лучше определить все тяжёлые зависимости (даже если они здесь не используются),
  * чтобы при webpack'овском code-splitting'е они не попадали в каждый чанк по отдельности
  */
+
+/** Но `babel-polyfill` надо ставить первым в каждой точке входа */
+import 'babel-polyfill';
 import _ from 'lodash';
 import Vue from 'vue';
 import FastClick from 'fastclick';
 import { isDevelopment } from './config';
 import { assert, uncaughtExceptionHandler, unhandledRejectionHandler } from './utils';
-import * as services from './services';
 import { HttpError, RequestError, ResponseError } from './services/http';
 import { reportError } from './api';
 
@@ -55,17 +56,14 @@ assert(app.$storage.enabled, 'Пожалуйста, выйдите из прив
 
 /**
  * Вешаем событие FastClick'а
+ * @link http://v4-alpha.getbootstrap.com/content/reboot/#click-delay-optimization-for-touch
  */
-// http://v4-alpha.getbootstrap.com/content/reboot/#click-delay-optimization-for-touch
-if ('addEventListener' in document) {
-  document.addEventListener('DOMContentLoaded', () => FastClick.attach(document.body), false);
-}
+document.addEventListener('DOMContentLoaded', () => FastClick.attach(document.body), false);
 
 /** Монтируем приложение */
 app.$mount('#app');
 
-/** Для разработки выкидываем его глобально */
+/** А для разработки выкидываем его глобально */
 if (process.env.NODE_ENV == 'development') {
   window.app = app;
 }
-
